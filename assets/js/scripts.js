464c60ef08f1c20e3fe22101458bff6ca07facdd
adam181189 <adam181189@gmail.com>
2020-08-26 16:54:56 +0000

    set up JS of initial landing page as the webpage is first loaded, only the first card will be displayed

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+/* On initiate */  /* On initiate */  
+
+ function initialising() {
+  document.getElementById("league-levels").style.display = "block";
+  document.getElementById("game-rules1").style.display = "none";
+  document.getElementById("game-rules2").style.display = "none";
+  document.getElementById("game-rules3").style.display = "none";
+  document.getElementById("game-rules4").style.display = "none";
+  document.getElementById("game-rules5").style.display = "none";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+};
+
+window.onload = initialising;
+
+   
