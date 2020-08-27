171b3fcbc8fb615699ce498869c54a61d6c7afe0
adam181189 <adam181189@gmail.com>
2020-08-27 10:25:02 +0000

    gave buttons a cursor instead of I-Beam, set up the How to Play feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,4 +14,82 @@
 
 window.onload = initialising;
 
-   
+/* button behaviour */ 
+$('.button').hover(function(){          
+    $(this).css('cursor','pointer');
+    return false;       
+});
+
+/* How to play Slides */
+
+    $("#rules").click(function() {
+  document.getElementById("league-levels").style.display = "none";
+  document.getElementById("game-rules1").style.display = "block";
+  document.getElementById("game-rules2").style.display = "none";
+  document.getElementById("game-rules3").style.display = "none";
+  document.getElementById("game-rules4").style.display = "none";
+  document.getElementById("game-rules5").style.display = "none";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+});
+
+ $("#game-rules1").click(function() {
+  document.getElementById("league-levels").style.display = "none";
+  document.getElementById("game-rules1").style.display = "none";
+  document.getElementById("game-rules2").style.display = "block";
+  document.getElementById("game-rules3").style.display = "none";
+  document.getElementById("game-rules4").style.display = "none";
+  document.getElementById("game-rules5").style.display = "none";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+});
+
+ $("#game-rules2").click(function() {
+  document.getElementById("league-levels").style.display = "none";
+  document.getElementById("game-rules1").style.display = "none";
+  document.getElementById("game-rules2").style.display = "none";
+  document.getElementById("game-rules3").style.display = "block";
+  document.getElementById("game-rules4").style.display = "none";
+  document.getElementById("game-rules5").style.display = "none";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+});
+
+$("#game-rules3").click(function() {
+  document.getElementById("league-levels").style.display = "none";
+  document.getElementById("game-rules1").style.display = "none";
+  document.getElementById("game-rules2").style.display = "none";
+  document.getElementById("game-rules3").style.display = "none";
+  document.getElementById("game-rules4").style.display = "block";
+  document.getElementById("game-rules5").style.display = "none";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+});
+
+$("#game-rules4").click(function() {
+  document.getElementById("league-levels").style.display = "none";
+  document.getElementById("game-rules1").style.display = "none";
+  document.getElementById("game-rules2").style.display = "none";
+  document.getElementById("game-rules3").style.display = "none";
+  document.getElementById("game-rules4").style.display = "none";
+  document.getElementById("game-rules5").style.display = "block";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+});
+
+$("#game-rules5").click(function() {
+  document.getElementById("league-levels").style.display = "block";
+  document.getElementById("game-rules1").style.display = "none";
+  document.getElementById("game-rules2").style.display = "none";
+  document.getElementById("game-rules3").style.display = "none";
+  document.getElementById("game-rules4").style.display = "none";
+  document.getElementById("game-rules5").style.display = "none";
+  document.getElementById("league-pokeball").style.display = "none";
+  document.getElementById("league-greatball").style.display = "none";
+  document.getElementById("league-masterball").style.display = "none";
+});
