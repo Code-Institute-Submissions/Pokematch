e7f2878eedb7be5d85089bfcecc5a83cbbe2445d
adam181189 <adam181189@gmail.com>
2020-08-28 12:47:49 +0000

    tidied up code by creating a function for all the cards set to none so I only needed the active card on each card related function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,132 +1,81 @@
-/* On initiate */ 
+/* Card calling */
+function cardLevels() {
+    document.getElementById("league-levels").style.display = "none";
+    document.getElementById("game-rules1").style.display = "none";
+    document.getElementById("game-rules2").style.display = "none";
+    document.getElementById("game-rules3").style.display = "none";
+    document.getElementById("game-rules4").style.display = "none";
+    document.getElementById("game-rules5").style.display = "none";
+    document.getElementById("league-pokeball").style.display = "none";
+    document.getElementById("league-greatball").style.display = "none";
+    document.getElementById("league-masterball").style.display = "none";
+}
+
+/* On initiate */
 
 function initialising() {
-  document.getElementById("league-levels").style.display = "block";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+    cardLevels();
+    document.getElementById("league-levels").style.display = "block";
 }
 
 window.onload = initialising;
 
 /* How to play Cards */
 
-$("#rules").click(function () {
-  document.getElementById("league-levels").style.display = "none";
-  document.getElementById("game-rules1").style.display = "block";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#rules").click(function() {
+    cardLevels();
+    document.getElementById("game-rules1").style.display = "block";
 });
 
-$("#next_button1").click(function () {
-  document.getElementById("league-levels").style.display = "none";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "block";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#next_button1").click(function() {
+    cardLevels();
+    document.getElementById("game-rules2").style.display = "block";
 });
 
-$("#next_button2").click(function () {
-  document.getElementById("league-levels").style.display = "none";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "block";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#next_button2").click(function() {
+    cardLevels();
+    document.getElementById("game-rules3").style.display = "block";
 });
 
-$("#next_button3").click(function () {
-  document.getElementById("league-levels").style.display = "none";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "block";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#next_button3").click(function() {
+    cardLevels();
+    document.getElementById("game-rules4").style.display = "block";
 });
 
-$("#next_button4").click(function () {
-  document.getElementById("league-levels").style.display = "none";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "block";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#next_button4").click(function() {
+    cardLevels();
+    document.getElementById("game-rules5").style.display = "block";
 });
 
-$("#finish_to_title").click(function () {
-  document.getElementById("league-levels").style.display = "block";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#finish_to_title").click(function() {
+    cardLevels();
+    document.getElementById("league-levels").style.display = "block";
 });
 
-$(".return-button").click(function () {
-  difficultySelect = undefined;
-  document.getElementById("league-levels").style.display = "block";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$(".return-button").click(function() {
+    difficultySelect = undefined;
+    cardLevels();
+    document.getElementById("league-levels").style.display = "block";
 });
 
 /* Game mode difficulty initialiser */
 
 let difficultySelect = null;
-$(".ball-levels").click(function () {
-  difficultySelect = $(this).attr("id");
+$(".ball-levels").click(function() {
+    difficultySelect = $(this).attr("id");
 });
 
-$("#start").click(function () {
-  document.getElementById("league-levels").style.display = "none";
-  document.getElementById("game-rules1").style.display = "none";
-  document.getElementById("game-rules2").style.display = "none";
-  document.getElementById("game-rules3").style.display = "none";
-  document.getElementById("game-rules4").style.display = "none";
-  document.getElementById("game-rules5").style.display = "none";
-  document.getElementById("league-pokeball").style.display = "none";
-  document.getElementById("league-greatball").style.display = "none";
-  document.getElementById("league-masterball").style.display = "none";
+$("#start").click(function() {
+    cardLevels();
 
-  if (difficultySelect == "pokeball") {
-    document.getElementById("league-pokeball").style.display = "block";
-  } else if (difficultySelect == "greatball") {
-    document.getElementById("league-greatball").style.display = "block";
-  } else if (difficultySelect == "masterball") {
-    document.getElementById("league-masterball").style.display = "block";
-  }
-    else 
-    document.getElementById("league-levels").style.display = "block";
+    if (difficultySelect == "pokeball") {
+        document.getElementById("league-pokeball").style.display = "block";
+    } else if (difficultySelect == "greatball") {
+        document.getElementById("league-greatball").style.display = "block";
+    } else if (difficultySelect == "masterball") {
+        document.getElementById("league-masterball").style.display = "block";
+    } else {
+        document.getElementById("league-levels").style.display = "block";
+    }
 });
 
